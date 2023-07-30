ffb4ae90e42bb31cbf1f9e0bfec2ae6cd74c70b4
Rz-min <rui.iizumi.20@gmail.com>
2023-07-30 22:09:31 +0900

    Environment skelton

---
+++ Rust-env/src/main.rs
@@ -0,0 +1,15 @@
+// Leet Code Environment
+pub struct Solution;
+
+impl Solution {
+    pub fn check() {
+        println!("Link");
+    }
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    }
+}
+
+fn main() {
+    // Solution::check();
+    assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+}
